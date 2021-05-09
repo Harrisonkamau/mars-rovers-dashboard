3dbf147d03bf0eae6bfc2597efa8a0ce4c7b95e3
Harrisonkamau <kamauharrison87@gmail.com>
2021-05-09 20:42:29 +0300

    feat: setup Eslint - Setup Eslint - Configure pre-commit hook to run yarn lint

--- src/server/routes.js
+++ src/server/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Mars Rover Dashboard server '});
+  res.json({ message: 'Welcome to Mars Rover Dashboard server' });
 });
 
 module.exports = router;
