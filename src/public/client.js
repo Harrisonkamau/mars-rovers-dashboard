0148e982e30a6ff465c3661a9fc51846d8611330
Harrisonkamau <kamauharrison87@gmail.com>
2021-05-17 07:58:51 +0300

    feat: Create selection bar & render it - Use a pure function to create a Selection Bar component and render it

--- src/public/client.js
+++ src/public/client.js
@@ -1,12 +1,27 @@
 const root = document.getElementById('root');
 
-const App = (state) => {
+const SelectionBar = ({ options, onChange }) => {
+  return `
+    <select onChange=${onChange}>
+      ${options.map(({ label, value }) => `<option value=${value}>${label}</option>`)}
+    </select>
+  `;
+};
 
+const options = ['Curiosity', 'Spirit', 'Opportunity', 'Perserverance'].map((item) => ({
+  label: item,
+  value: item.toLowerCase(),
+}));
+
+const handleOnChange = (event) => console.log('event', event.target);
+
+const App = (state) => {
   return `
     <header>${state.name || 'Mars Rover'}</header>
     <main>
       <section>
         <h3>Rover Name</h3>
+        ${SelectionBar({ options, onChange: handleOnChange })}
       </section>
     </main>
   `;
